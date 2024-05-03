24875ed4d864116889043f394a7c420c3f495081
AinhoaMaya <ainhoamaya.web@gmail.com>
2024-05-03 09:44:17 +0200

    Extracción datos de licencia

--- data/airbnb-filter-spain.js
+++ data/airbnb-filter-spain.js
@@ -11,27 +11,29 @@
   const response = await fetch('https://catalegdades.caib.cat/api/views/3q3t-usfm/rows.json?accessType=DOWNLOAD')
   this.data = await response.json()
 
+
 // Devolver los datos que contengan "Spain" con un bucle
   
-  locations.forEach (location => {
-    if (location.hostLocation && location.hostLocation.includes('Spain')) {
-      console.log(location)
-    }
-  })
+  // locations.forEach (location => {
+  //   if (location.hostLocation && location.hostLocation.includes('Spain')) {
+  //     console.log(location)
+  //   }
+  // })
 
 
-// Devolver los datos que contengan "Spain" con un filter y los que no contengan "Spain" con un filter
+// Devolver los datos que contengan "Spain" con un filter
 
-  // const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
+  const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
 
-  // return { filterSpain, foreigner };
+  return filterSpain;
+
 
 // Devolver los datos que no contengan "Spain" con un filter
 
-//   const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
+  // const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
 
-//   return foreigner;
-// })()
+  // return foreigner;
+
 
 // Porcentaje de plazas que tienen los Españoles con reduce
 
@@ -63,15 +65,15 @@ const percentatgeForeigners = (locations) => {
   const results = locations.reduce((acc, location) => {
     acc.total++;
 
-    if (location.host_location && location.host_location.includes('Spain')) {
-      acc.spainCount++;
+    if (location.host_location && !location.host_location.includes('Spain')) {
+      acc.foreingCount++;
     }
     return acc;
-  }, { spainCount: 0, total: 0 });
+  }, { foreingCount: 0, total: 0 });
 
 
   if (results.total > 0) {
-    const percentage = (results.spainCount / results.total) * 100;
+    const percentage = (results.foreingCount / results.total) * 100;
     return percentage.toFixed(2) + '%';
   } else {
     return '0%';
@@ -82,35 +84,3 @@ const percentatgeForeignersList = percentatgeForeigners(locations);
 console.log(percentatgeForeignersList);
 
 })
-
-
-// 1.Filtrar "inside-airb.json" y sacar los que no son de España; 2.Filtrar: host_name y ubicacion; 3.Filtrar la licencia de los nombres de "host_name" y dejarlos en un JSON aparte 4.llamada fetch al CAIB y extraer el nombre del explotador (poner las licencias como un array) - mirar con includes si tiene la licencia y saca el nombre del explotador (la localicación GPS está en el inside, también sacarlo para luego localizarlo en un mapa)
-
-//para punto 1: map
-
-// EJERCICIO
-// 1º Partiendo de los gestores extranjeros:
-
-
-// const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
-//   return foreigner;
-// })()
-
-// // Busca en el archivo inside-airbnb.json qué propiedades tienen
-// const propertiesForeigners = foreigners.filter(foreigner => {
-//   return foreigner.some(foreign => foreign.hostId === property.hostId);
-// });
-
-// console.log(propertiesForeigners);
-
-
-// una vez hecho esto extrae de esos datos la licencia, latitude, longitude, neighbourhood_cleansed, host_name en nuevo array de objetos
-
-// 2º Conecta via fetch esta url 
-// https://catalegdades.caib.cat/api/views/3q3t-usfm/rows.json?accessType=DOWNLOAD
-
-  
-
-// Y busca por cada licencia de propiedad quién es su explotador (penúltimo dato de cada registro)
-
-// 3º Adjunta el nombre del explotador a el conjunto de datos que has escrito en el punto 1
--- data/inside-airbnb-filter.js
+++ data/inside-airbnb-filter.js
@@ -3,14 +3,52 @@
   const fs = require('fs/promises')
 
   const file = await fs.readFile('../data/inside-airbnb.json', 'utf-8')
+  const locations = JSON.parse(file)
+
+  // 2º Conecta via fetch esta url 
+  const fileData = await fs.readFile('https://catalegdades.caib.cat/api/views/3q3t-usfm/rows.json?accessType=DOWNLOAD')
   const data = JSON.parse(file)
 
+
+  // EJERCICIO
+  // Devolver los datos que contengan "Spain" con un filter
+
+  // const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
+
+  // return filterSpain;
+
+
+  // Devolver los datos que no contengan "Spain" con un filter
+  // 1º Partiendo de los gestores extranjeros:
+
+  // const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
+
+  // return foreigner;
+
+
+  // 1.1 Busca en el archivo inside-airbnb.json qué propiedades tienen
+  const propertiesForeigners = foreigners.filter(foreigner => {
+    return foreigner.some(foreign => foreign.hostId === property.hostId);
+  });
+
+  console.log(propertiesForeigners);
+
+
+  // 1.2 Una vez hecho esto extrae de esos datos la licencia, latitude, longitude, neighbourhood_cleansed, host_name en nuevo array de objetos
   const licenses = data.map (element => {
     return{ 
       poblacion: element.host_location,
       licencia: element.license,
+      longitude: element.longitude,
+      neighbourhood_cleansed: element.neighbourhood_cleansed,
+      host_name: host_name
     }
   })
 
   console.log(licenses)
+
+
+  // 1.3 Y busca por cada licencia de propiedad quién es su explotador (penúltimo dato de cada registro)
+
+  // 3º Adjunta el nombre del explotador a el conjunto de datos que has escrito en el punto 1
 })()
