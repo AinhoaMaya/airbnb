9069545b78e3f1debb53bce6e2256ae20f46506f
AinhoaMaya <ainhoamaya.web@gmail.com>
2024-05-03 14:01:59 +0200

    Saber de qué País viene cada uno

--- data/airbnb-filter-spain.js
+++ data/airbnb-filter-spain.js
@@ -5,8 +5,6 @@
   const file = await fs.readFile('../data/airbnb-owner-ranking-last-year.json', 'utf-8')
   const locations = JSON.parse(file)
 
-  const response = await fetch('https://catalegdades.caib.cat/api/views/3q3t-usfm/rows.json?accessType=DOWNLOAD')
-
 
   // Devolver los datos que contengan "Spain" con un bucle
   
@@ -20,24 +18,38 @@
   // Devolver los datos que contengan "Spain" con un filter
 
   const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
+  console.log(filterSpain)
 
 
   // Devolver los datos que no contengan "Spain" con un filter
 
   const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
+  console.log(foreigner)
 
 
   // Porcentaje de plazas que tienen los Españoles y los extranjeros con reduce
 
-  const percentatgeSpainPlaces = spanishOwners.reduce((acc, curr) => acc + parseFloat(curr.percentatgeSpainPlaces), 0);
+  // const percentatgeSpainPlaces = spanishOwners.reduce((acc, curr) => acc + parseFloat(curr.percentatgeSpainPlaces), 0);
 
-  const percentatgeForeignPlaces = foreignOwners.reduce((acc, curr) => acc + parseFloat(curr.percentatgeForeignPlaces), 0);
+  // const percentatgeForeignPlaces = foreignOwners.reduce((acc, curr) => acc + parseFloat(curr.percentatgeForeignPlaces), 0);
+
+  // console.log('Spanish owners:', percentatgeSpainPlaces.toFixed(2))
+  // console.log('Foreign owners:', percentatgeForeignPlaces.toFixed(2))
 
 
-  console.log('Spanish owners:', percentatgeSpainPlaces.toFixed(2))
-  console.log('Foreign owners:', percentatgeForeignPlaces.toFixed(2))
+  // Saber de qué País viene cada uno
+  
+  const foreignOwnerByCountry = foreigner.reduce((acc, owner) => {
+    const country = owner.hostLocation.split(', ')[1]
 
+    if (!acc[country]) {
+      acc[country] = parseFloat(owner.percentatgePlaces)
+    } else {
+      acc[country] += parseFloat(owner.percentatgePlaces)
+    }
 
-  // Saber de que País viene cada uno
+    return acc
+  }, {})
 
+  console.log(foreignOwnerByCountry)
 })()
