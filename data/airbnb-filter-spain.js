e5eb56840cbd98c6c3cee6498e4b9e228c3c99ab
AinhoaMaya <ainhoamaya.web@gmail.com>
2024-05-03 11:49:16 +0200

    Modificación porcentaje de plazas españoles y extranjeros

--- data/airbnb-filter-spain.js
+++ data/airbnb-filter-spain.js
@@ -5,14 +5,10 @@
   const file = await fs.readFile('../data/airbnb-owner-ranking-last-year.json', 'utf-8')
   const locations = JSON.parse(file)
 
-  // const files = await fs.readFile('../data/inside-airbnb-filter', 'utf-8')
-  // const locations = JSON.parse(files)
-
   const response = await fetch('https://catalegdades.caib.cat/api/views/3q3t-usfm/rows.json?accessType=DOWNLOAD')
-  this.data = await response.json()
 
 
-// Devolver los datos que contengan "Spain" con un bucle
+  // Devolver los datos que contengan "Spain" con un bucle
   
   // locations.forEach (location => {
   //   if (location.hostLocation && location.hostLocation.includes('Spain')) {
@@ -21,66 +17,27 @@
   // })
 
 
-// Devolver los datos que contengan "Spain" con un filter
+  // Devolver los datos que contengan "Spain" con un filter
 
-  // const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
-
-  // return filterSpain;
+  const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
 
 
-// Devolver los datos que no contengan "Spain" con un filter
+  // Devolver los datos que no contengan "Spain" con un filter
 
-  // const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
-
-  // return foreigner;
+  const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
 
 
-// Porcentaje de plazas que tienen los Españoles con reduce
+  // Porcentaje de plazas que tienen los Españoles y los extranjeros con reduce
 
-const percentatgeSpain = (locations) => {
-  const results = locations.reduce((acc, location) => {
-    acc.total++;
+  const percentatgeSpainPlaces = spanishOwners.reduce((acc, curr) => acc + parseFloat(curr.percentatgeSpainPlaces), 0);
 
-    if (location.host_location && location.host_location.includes('Spain')) {
-      acc.spainCount++;
-    }
-    return acc;
-  }, { spainCount: 0, total: 0 });
+  const percentatgeForeignPlaces = foreignOwners.reduce((acc, curr) => acc + parseFloat(curr.percentatgeForeignPlaces), 0);
 
 
-  if (results.total > 0) {
-    const percentage = (results.spainCount / results.total) * 100;
-    return percentage.toFixed(2) + '%';
-  } else {
-    return '0%';
-  }
-};
-
-const percentatgeSpainList = percentatgeSpain(locations);
-console.log(percentatgeSpainList);
+  console.log('Spanish owners:', percentatgeSpainPlaces.toFixed(2))
+  console.log('Foreign owners:', percentatgeForeignPlaces.toFixed(2))
 
 
-// Porcentaje de plazas que tienen los extranjeros con reduce
-const percentatgeForeigners = (locations) => {
-  const results = locations.reduce((acc, location) => {
-    acc.total++;
+  // Saber de que País viene cada uno
 
-    if (location.host_location && !location.host_location.includes('Spain')) {
-      acc.foreingCount++;
-    }
-    return acc;
-  }, { foreingCount: 0, total: 0 });
-
-
-  if (results.total > 0) {
-    const percentage = (results.foreingCount / results.total) * 100;
-    return percentage.toFixed(2) + '%';
-  } else {
-    return '0%';
-  }
-};
-
-const percentatgeForeignersList = percentatgeForeigners(locations);
-console.log(percentatgeForeignersList);
-
-})
+})()
