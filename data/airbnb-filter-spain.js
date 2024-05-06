cd8098d4c8c7c6613994663084c1cb6b57d542bb
AinhoaMaya <ainhoamaya.web@gmail.com>
2024-05-06 09:13:07 +0200

    Recorrer el array, y crear un objeto donde cada una de las claves sea la fecha y hacer la suma total de los valores de los años con "forEach"

--- data/airbnb-filter-spain.js
+++ data/airbnb-filter-spain.js
@@ -5,6 +5,9 @@
   const file = await fs.readFile('../data/airbnb-owner-ranking-last-year.json', 'utf-8')
   const locations = JSON.parse(file)
 
+  const files = await fs.readFile('../data/property-transmissions.json', 'utf-8')
+  const property = JSON.parse(files)
+
 
   // Devolver los datos que contengan "Spain" con un bucle
   
@@ -17,14 +20,14 @@
 
   // Devolver los datos que contengan "Spain" con un filter
 
-  const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
-  console.log(filterSpain)
+  // const filterSpain = locations.filter(location => location.hostLocation && location.hostLocation.includes('Spain'));
+  // console.log(filterSpain)
 
 
-  // Devolver los datos que no contengan "Spain" con un filter
+  // // Devolver los datos que no contengan "Spain" con un filter
 
-  const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
-  console.log(foreigner)
+  // const foreigner = locations.filter(location => location.hostLocation && !location.hostLocation.includes('Spain'));
+  // console.log(foreigner)
 
 
   // Porcentaje de plazas que tienen los Españoles y los extranjeros con reduce
@@ -39,23 +42,50 @@
 
   // Saber de qué País viene cada uno
   
-  const foreignOwnerByCountry = foreigner.reduce((acc, owner) => {
-    const country = owner.hostLocation.split(', ')[1]
+  // const foreignOwnerByCountry = foreigner.reduce((acc, owner) => {
+  //   const country = owner.hostLocation.split(', ')[1]
 
-    if (!acc[country]) {
-      acc[country] = parseFloat(owner.percentatgePlaces)
-    } else {
-      acc[country] += parseFloat(owner.percentatgePlaces)
-    }
+  //   if (!acc[country]) {
+  //     acc[country] = parseFloat(owner.percentatgePlaces)
+  //   } else {
+  //     acc[country] += parseFloat(owner.percentatgePlaces)
+  //   }
 
-    return acc
-  }, {})
+  //   return acc
+  // }, {})
 
-  console.log(foreignOwnerByCountry)
+  // console.log(foreignOwnerByCountry)
 
-  
+
   // Ordenar por hostLocation
-  const ownerBedRankings = data.sort((a, b) => a.hostLocation.localeCompare(b.hostLocation))
 
-  console.log(ownerBedRankings)
+  // const ownerBedRankings = data.sort((a, b) => a.hostLocation.localeCompare(b.hostLocation))
+
+  // console.log(ownerBedRankings)
+
+
+  // Recorrer el array, y crear un objeto donde cada una de las claves sea la fecha y hacer la suma total de los valores de los años con "forEach"
+
+  const dataProperty = properties => {
+    const resultado = {};
+
+    properties.alaior.forEach(property => {
+      const year = property.date.split('-')[0];
+      const value = parseInt(property.value, 10);
+
+      if (!resultado[year]) {
+        resultado[year] = 0;
+      }
+
+      resultado[year] += value;
+    });
+
+    return { alaior: resultado };
+  }
+
+  const resultadoFinal = dataProperty(property);
+  console.log(resultadoFinal);
+
+  // Recorrer el array, y crear un objeto donde cada una de las claves sea la fecha y hacer la suma total de los valores de los años con "reduce"
+
 })()
