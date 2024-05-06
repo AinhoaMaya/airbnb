32e15c5338921696cc103d5293a3611a85d6e5e6
AinhoaMaya <ainhoamaya.web@gmail.com>
2024-05-06 09:45:13 +0200

    Recorrer el array, y crear un objeto donde cada una de las claves sea la fecha y hacer la suma total de los valores de los años con "reduce"

--- data/airbnb-filter-spain.js
+++ data/airbnb-filter-spain.js
@@ -66,26 +66,81 @@
 
   // Recorrer el array, y crear un objeto donde cada una de las claves sea la fecha y hacer la suma total de los valores de los años con "forEach"
 
+  // const dataProperty = properties => {
+  //   const resultado = {};
+
+  //   properties.alaior.forEach(property => {
+  //     const year = property.date.split('-')[0];
+  //     const value = parseInt(property.value, 10);
+
+  //     if (!resultado[year]) {
+  //       resultado[year] = 0;
+  //     }
+
+  //     resultado[year] += value;
+  //   });
+
+  //   return { alaior: resultado };
+  // }
+
+  // const resultadoFinal = dataProperty(property);
+  // console.log(resultadoFinal);
+
+  // Recorrer el array, y crear un objeto donde cada una de las claves sea la fecha y hacer la suma total de los valores de los años con "reduce"
+  // try{
+  //   const towns = {}
+
+  //   Object.entries(data).forEach(([key, value]) => {
+  //     const years = value.reduce((acc, element) => {
+  //       const year = element.data.split('-')[0]
+        
+  //       if (!acc[year]) {
+  //         acc[year] = parseFloat(element.value)
+  //       } else {
+  //         acc[year] += parseFloat(element.value)
+  //       }
+
+  //       return acc
+  //     }, {})
+
+  //     towns[key] = years // en cada vuelta de towns, se crea una clave, cuyo valor es "years"
+
+  //     console.log(years)
+  //     console.log(key)
+  //   })
+
+
+  // } catch (error) {
+  //   console.log(error)
+  // }
+
   const dataProperty = properties => {
-    const resultado = {};
+    // Utilizamos reduce para acumular los resultados en un objeto
+    const resultado = properties.alaior.reduce((acc, property) => {
+      const year = property.date.split('-')[0]; // Extraemos el año de la fecha
+      const value = parseInt(property.value, 10); // Convertimos el valor a número
 
-    properties.alaior.forEach(property => {
-      const year = property.date.split('-')[0];
-      const value = parseInt(property.value, 10);
-
-      if (!resultado[year]) {
-        resultado[year] = 0;
+      // Inicializamos el año en el acumulador si no existe aún
+      if (!acc[year]) {
+          acc[year] = 0;
       }
 
-      resultado[year] += value;
-    });
+      acc[year] += value; // Sumamos el valor al total del año correspondiente
+      return acc; // Retornamos el acumulador actualizado para la siguiente iteración
+    }, {}); // El objeto vacío {} es el valor inicial del acumulador acc
 
     return { alaior: resultado };
-  }
+  };
 
   const resultadoFinal = dataProperty(property);
   console.log(resultadoFinal);
 
-  // Recorrer el array, y crear un objeto donde cada una de las claves sea la fecha y hacer la suma total de los valores de los años con "reduce"
+  // Crear un nuevo json a partir de los resultados
+  try {
+    await fs.writeFile('../data/result-years.json', JSON.stringify(resultadoFinal, null, 2), 'utf8');
+    console.log("El archivo JSON fue guardado correctamente.");
+  } catch (error) {
+    console.error("Error al escribir el archivo JSON:", error);
+  }
 
 })()
